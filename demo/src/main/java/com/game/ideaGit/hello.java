8f752bf99a8f506874ae652c83e306c1ce2f0854
jack <864234808@qq.com>
2020-09-12 15:28:29 +0800

    分支添加

--- demo/src/main/java/com/game/ideaGit/hello.java
+++ demo/src/main/java/com/game/ideaGit/hello.java
@@ -7,5 +7,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello idea Git !");
         System.out.println("第一次修改");
+        System.out.println("dev分支添加");
     }
 }

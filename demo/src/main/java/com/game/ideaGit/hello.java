74a967cfeceab8073e8c3bc5a67289199a06bcdb
jack <864234808@qq.com>
2020-09-12 15:21:56 +0800

    添加文件

---
+++ demo/src/main/java/com/game/ideaGit/hello.java
@@ -0,0 +1,10 @@
+package com.game.ideaGit;
+
+/**
+ * @date 2020/9/12 15:16
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello idea Git !");
+    }
+}
